c76b9dfebcfd07806e8a890f5e4d420eab647649
nishadinfo09 <mdnishadali03@gmail.com>
2025-12-11 21:34:43 +0600

    add production origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://localchefbazaar-backend-production.up.railway.app",
+  "https://localchefbazaar.netlify.app",
 ];
 
 app.use(
