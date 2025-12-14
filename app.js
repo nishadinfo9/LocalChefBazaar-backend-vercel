557c0d126e2508be591127ec9033c49b353ca9de
nishadinfo09 <mdnishadali03@gmail.com>
2025-12-14 09:17:48 +0600

    Update allowedOrigins to use the correct production URL

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://localchefbazar.netlify.app",
+  "https://chef-bazaar.vercel.app",
 ];
 
 app.use(
